2c086eff6776c3488b5513b6cdb73afa8a9cb02c
neostrange <ali.hur57@gmail.com>
2015-10-30 00:53:30 -0700

    Some new classes have been added.

---
+++ TestES/src/main/java/org/aserg/estest/Connection.java
@@ -0,0 +1,16 @@
+package org.aserg.estest;
+
+public class Connection {
+	
+	
+	
+	private Download download;
+	private VirusTotal virusTotal;
+	private VirusTotalScan virusTotalscan;
+	
+	
+	
+	
+	
+
+}
---
+++ TestES/src/main/java/org/aserg/estest/VirusTotal.java
@@ -0,0 +1,5 @@
+package org.aserg.estest;
+
+public class VirusTotal {
+
+}
