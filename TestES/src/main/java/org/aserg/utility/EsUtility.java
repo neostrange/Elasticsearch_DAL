a9752ec26598d99b23daeb883e222480043517fe
Waseem <waseem.farooqui19@gmail.com>
2015-11-04 21:07:55 +0500

    Added Property File with Queries

---
+++ TestES/src/main/java/org/aserg/utility/EsUtility.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.aserg.utility;
+
+/**
+ * @author Waseem
+ *
+ */
+public class EsUtility {
+
+	/**
+	 * 
+	 */
+	public EsUtility() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- TestES/src/main/java/org/aserg/utility/SqlUtility.java
+++ TestES/src/main/java/org/aserg/utility/SqlUtility.java
@@ -1,13 +1,33 @@
 package org.aserg.utility;
 
-public class SqlUtility {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
-	final static String NETWORK_LAYER_INCIDENT_QUERY = "";
-	final static String SSH_INCIDENT_QUERY = "";
-	final static String MYSQL_INCIDENT_QUERY = "";
-	final static String MSSQL_INCIDENT_QUERY = "";
-	final static String WEB_INCIDENT_QUERY = "";
-	final static String SIP_INCIDENT_QUERY = "";
-	final static String MALWARE_INCIDENT_QUERY = "";
+public class SqlUtility {
+	final static String BASE_PATH = System.getProperty("user.dir")+"/config/";
+	Properties p = new Properties();
 	
+	public SqlUtility() {
+	}
+	
+	public Properties getproperty(String config){
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(config));
+			p.load(br);
+		} catch(FileNotFoundException e){
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return p;	
+	}
+	public static void main (String args[]){
+		System.out.print(new SqlUtility().getproperty(BASE_PATH+"Query.properties").getProperty("MALWARE_INCIDENT_QUERY"));
+	}
+				
 }
