db1dee5445a6ebaf60b8fa8a7f27d1c706d68809
neostrange <ali.hur57@gmail.com>
2015-11-04 03:48:32 -0800

    Just added a dummy clas SshIncident.

---
+++ TestES/src/main/java/org/aserg/dal/SshIncidentPopulate.java
@@ -0,0 +1,36 @@
+package org.aserg.dal;
+
+public class SshIncidentPopulate {
+	
+	final static String sshIncidentQuery = "";
+	
+	private DbUtility dbUtility;
+	private ResultSet resultSet;
+	
+	// domain objects to be initialized here
+	
+	
+	public SshIncidentPopulate(){
+		
+	}
+	
+	
+	public Incident populate(ResultSet resultSet){
+		
+		// logic to populate SshIncident domain objects
+		
+	}
+	
+	
+	public static void main(String args[]){
+		
+		IncidentPopulate incdentPopulate;
+		
+		
+		incidentPopulate = new SshIncidentPopulate();
+		incidentPopulate.populate();
+	}
+	
+	
+
+}
