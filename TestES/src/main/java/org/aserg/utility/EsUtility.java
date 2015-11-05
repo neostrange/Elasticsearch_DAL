2df4e1a91c05a40b9fdcb416a8763a5c1c593c61
YG <yumnaghazi@gmail.com>
2015-11-06 00:45:24 +0500

    EsUtility with BulkProcessor - the initial version.

--- TestES/src/main/java/org/aserg/utility/EsUtility.java
+++ TestES/src/main/java/org/aserg/utility/EsUtility.java
@@ -1,19 +1,101 @@
-/**
- * 
- */
 package org.aserg.utility;
 
-/**
- * @author Waseem
- *
- */
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
+
 public class EsUtility {
 
 	/**
 	 * 
 	 */
+	private ESLogger logger;
+	private TransportClient client;
+
+	private String DEFAULT_HOSTNAME;
+	private String DEFAULT_CLUSTERNAME;
+	private int DEFAULT_PORT;
+
+	private int DEFAULT_CONCURRENT_REQUESTS;
+	private int DEFAULT_BULK_ACTIONS;
+	private TimeValue DEFAULT_FLUSH_INTERVAL;
+	public ByteSizeValue DEFAULT_BULK_SIZE;
+
+	private BulkProcessor bulkProcessor;
+
+	private final BulkProcessor.Listener listener = new BulkProcessor.Listener() {
+
+		public void beforeBulk(long executionId, BulkRequest request) {
+			// TODO log
+		}
+
+		public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+			// TODO log
+		}
+
+		public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+			if (response.hasFailures()) {
+				throw new RuntimeException(response.buildFailureMessage());
+			}
+		}
+	};
+
 	public EsUtility() {
-		// TODO Auto-generated constructor stub
+		logger = Loggers.getLogger(EsUtility.class);
+		// Load ES Properties
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream("config/es.properties"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// initialize variables from properties file
+		DEFAULT_HOSTNAME = prop.getProperty("es.hostname");
+		DEFAULT_CLUSTERNAME = prop.getProperty("es.clustername");
+		DEFAULT_PORT = Integer.parseInt(prop.getProperty("es.port"));
+		DEFAULT_CONCURRENT_REQUESTS = Integer.parseInt(prop.getProperty("es.concurrentrequests"));
+		DEFAULT_BULK_ACTIONS = Integer.parseInt(prop.getProperty("es.bulkactions"));
+		DEFAULT_FLUSH_INTERVAL = TimeValue.timeValueSeconds(Integer.parseInt(prop.getProperty("es.flushinterval")));
+		DEFAULT_BULK_SIZE = new ByteSizeValue(Integer.parseInt(prop.getProperty("es.bulksize")), ByteSizeUnit.MB);
+		// initialize ES TransportClient and bulkProcessor
+		client = new TransportClient();
+		client.addTransportAddress(new InetSocketTransportAddress(DEFAULT_HOSTNAME, DEFAULT_PORT));
+		bulkProcessor = BulkProcessor.builder(client, listener).setBulkActions(DEFAULT_BULK_ACTIONS)
+				.setConcurrentRequests(DEFAULT_CONCURRENT_REQUESTS).setFlushInterval(DEFAULT_FLUSH_INTERVAL)
+				.setBulkSize(DEFAULT_BULK_SIZE).build();
+
+	}
+
+	public BulkProcessor getBulkProcessor() {
+		return bulkProcessor;
+	}
+
+	public void setBulkProcessor(BulkProcessor bulkProcessor) {
+		this.bulkProcessor = bulkProcessor;
+	}
+
+	public TransportClient getClient() {
+		return client;
+	}
+
+	public void setClient(TransportClient client) {
+		this.client = client;
 	}
 
 }
