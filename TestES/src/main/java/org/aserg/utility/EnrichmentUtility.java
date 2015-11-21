70fe0449819538780d05fd736930e74ba32231fa
Waseem <waseem.farooqui19@gmail.com>
2015-11-21 21:31:28 +0500

    Changes by the configurations

--- TestES/src/main/java/org/aserg/utility/EnrichmentUtility.java
+++ TestES/src/main/java/org/aserg/utility/EnrichmentUtility.java
@@ -31,7 +31,7 @@ public class EnrichmentUtility {
 	public static Origin getOrigin(String remote_host) {
 		try {
 
-			String dir = "E:/TI/GeoLiteCity.dat";
+			String dir = IOFileUtility.readTime("GEOIP_FILE_PATH_LINUX");
 			LookupService cl = new LookupService(dir, LookupService.GEOIP_MEMORY_CACHE);
 			Location loc = cl.getLocation(remote_host);
 			if(loc != null){
